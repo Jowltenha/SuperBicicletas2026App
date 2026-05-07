d16d4b6a9fec99aadbdfd939daf5dd14d89a7e55
João Pedro Barboza <joaopedro.rigoto@gmail.com>
2026-05-07 08:30:30 -0300

    feat

---
+++ SuperBicicletas2026App/Pages/AboutUs.cshtml.cs
@@ -0,0 +1,12 @@
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+
+namespace SuperBicicletas2026App.Pages
+{
+    public class AboutUsModel : PageModel
+    {
+        public void OnGet()
+        {
+        }
+    }
+}
