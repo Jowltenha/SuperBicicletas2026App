229b06ab652c3f337e522eaaf4563351e606ffaa
João Pedro Barboza <joaopedro.rigoto@gmail.com>
2026-05-08 09:15:33 -0300

    feat, foi feito card das bikes para futuramente ser adicionado detalhamentos.

---
+++ SuperBicicletas2026App/Models/Bicicleta.cs
@@ -0,0 +1,13 @@
+﻿namespace SuperBicicletas2026App.Models
+{
+    public class Bicicleta
+    {
+        public int BikeId { get; set; }
+        public string Nome { get; set; }
+        public string Descricao { get; set; }
+        public string ImagemUri { get; set; }
+        public double Preco { get; set; }
+        public bool EntregaExpressa { get; set; }
+        public DateTime DataCadastro { get; set; }
+    }
+}
--- SuperBicicletas2026App/Pages/Index.cshtml.cs
+++ SuperBicicletas2026App/Pages/Index.cshtml.cs
@@ -1,12 +1,13 @@
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.AspNetCore.Mvc.RazorPages;
+using SuperBicicletas2026App.Models;
 
 namespace SuperBicicletas2026App.Pages
 {
     public class IndexModel : PageModel
     {
         private readonly ILogger<IndexModel> _logger;
-
+        public IList<Bicicleta> Bicicletas { get; set; }
         public IndexModel(ILogger<IndexModel> logger)
         {
             _logger = logger;
@@ -14,7 +15,16 @@ namespace SuperBicicletas2026App.Pages
 
         public void OnGet()
         {
+            Bicicletas = new List<Bicicleta>
+            {
+                new Bicicleta { BikeId = 1, Nome = "Montain Bike Caloi", Descricao = "Para aventuras e trilhas", ImagemUri = "/imagens/bikeMontainCaloi.png", DataCadastro = DateTime.Now, EntregaExpressa = true, Preco = 6500.99 },
 
+                new Bicicleta { BikeId = 2, Nome = "Montain Bike Oggi", Descricao = "Para trilhas e aventuras", ImagemUri = "/imagens/bikeMontainOggi.png", DataCadastro = DateTime.Now, EntregaExpressa = true, Preco = 5999.99 },
+
+                new Bicicleta { BikeId = 3, Nome = "Bike Speed Specialized", Descricao = "Para competição e treinos na estradas", ImagemUri = "/imagens/bikeSpeedSP.png", DataCadastro = DateTime.Now, EntregaExpressa = true, Preco = 6999.99 },
+
+                new Bicicleta { BikeId = 4, Nome = "Bike Speed Trek", Descricao = "Para estradas", ImagemUri = "/imagens/bikeSpeedTrek.png",DataCadastro = DateTime.Now, EntregaExpressa = true, Preco = 8500.99 }
+            };
         }
     }
 }
